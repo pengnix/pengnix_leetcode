fbd0e09f177154041aa680167aaeed0af09e4e1b
pengnix <superpengnix@gmail.com>
2018-10-10 20:41:20 +0800

    errer

--- pengnix_leetcode/CPP/p1/Solution.cpp
+++ pengnix_leetcode/CPP/p1/Solution.cpp
@@ -1,6 +1,9 @@
 #include "Solution.h"
 #include <vector>
+#include <map>
+#include <iostream>
 using std::vector;
+using std::map;
 
 Solution::Solution()
 {
@@ -13,7 +16,24 @@ Solution::~Solution()
 
 vector<int> Solution::twoSum(vector<int>& nums, int target) {
 	vector<int> vec(2);
-	vec[0] = 0;
-	vec[1] = 1;
+	map<int, int> map;
+	int index = 0;
+	int vec_len = nums.size();
+	for (auto it = nums.begin(); it != nums.end(); it++,index++) {
+		map[*it]=index;
+		std::cout <<"map"<< *it << ": " << index << std::endl;
+	}
+	index = 0;
+	for (auto it = nums.begin(); index<vec_len; it++, index++) {
+		int left = target - *it;
+		std::cout << *it << " " << left << std::endl;
+		auto result = map.find(left);
+		if (result != map.end()) {
+			vec[0] = index;
+			vec[1] = result->second;
+			std::cout << vec[0] << ": " << vec[1] <<"?"<<*it<< std::endl;
+			break;
+		}
+	}
 	return vec;
 }
