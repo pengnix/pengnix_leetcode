d3724b89cb3b94a06c7df4a508f5255d8fc7e782
xuhaopeng <xuhaopeng@exruaho.com>
2018-10-12 16:31:58 +0800

    init p2

---
+++ pengnix_leetcode/CPP/p2/Solution.cpp
@@ -0,0 +1,5 @@
+#include "Solution.h"
+#include <iostream>
+    ListNode* Solution::addTwoNumbers(ListNode* l1, ListNode* l2) {
+        return NULL;
+    }
---
+++ pengnix_leetcode/CPP/p2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Solution.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	cout<<"output"<<endl;
+	return 0;
+}
